7f7d8b32c4d5f8e75c4146288d60612c97e84a00
Susan Su <suchangau@hotmail.com>
2021-03-14 14:11:33 +1100

    submit

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,70 +1,109 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-var allLetters = ['!','@','#','%','^','&','*''$','(',')',
-                  'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                  'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-                  '0','1','2','3','4','5','6','7','8','9'];
-var specialLetters = ['!','@','#','%','^','&','*''$','(',')'];
-var uppcaseLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var lowercaseLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var numberLetters = ['0','1','2','3','4','5','6','7','8','9'];
+var passwordEntry=document.getElementById("#password");
 
 
-var passwordLength ;
-var includeSpecialLetters;
-var includeNumberLetters;
-var includeUppcaseLetters;
-var includeLowercaseLetters;
+var caseArray = [];
+var specialChar = ['!','@','#','%','^','&','*','$','(',')',']','>','<','?','/'];
+var upperCaseChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numericChar = ['0','1','2','3','4','5','6','7','8','9'];
+
+
+var passwordLength;
+var includeSpecial;
+var includeNumeric;
+var includeUpperCase;
+var includeLowerCase;
 
 
 function generatePassword() {
  // generate password here
 
+ var passwordString = "";
+
+
   passwordLength = prompt("How many characters in password?");
   
-  if(passwordLength < 8 || passwordLength > 28){
+  if(passwordLength < 8 || passwordLength > 128){
     return "Password must be greater than 8 and less than 28";
   }
   
-  includeSpecialLetters = confirm("Do you need special characters?");
-  includeUppcaseLetters = confirm("Do you need uppcase characters");
-  includeLowercaseLetters = confirm("Do yu need lowercase characts?");
-  includeNumberLetters = confirm("Do you need numbers?");
-  // passrod legnth is 10;
-   var passwordString = "";
 
-   // You need to get 10 random letters from allLetters array
+  includeSpecial = confirm("Do you want to include special characters?");
+  includeUpperCase = confirm("Do you want to include uppercase characters");
+  includeLowerCase = confirm("Do you want to include lowercase characters?");
+  includeNumeric = confirm("Do you want to include numeric characters?");
+
+
+  if (includeLowerCase == false && includeUpperCase == false && includeNumeric == false && includeSpecial == false) {
+    alert("Please select one type of character");
+  includeSpecial = confirm("Do you want to include special characters?");
+  includeUpperCase = confirm("Do you want to include uppercase characters");
+  includeLowerCase = confirm("Do you want to include lowercase characters?");
+  includeNumeric = confirm("Do you want to include numeric characters?");
+   
+}
+
+if (useLowerCase == true){
+  caseArray.push(lowerCaseChar);
+
+}
+if (useUpperCase == true){
+  caseArray.push(upperCaseChar)
+
+}
+if (useNumeric == true){
+  caseArray.push(numericChar)
+  
+}
+if (useSpecial == true){
+  caseArray.push(specialChar)
+}
+  
+
    for(var i = 0; i < passwordLength; i++)
    {
-     var randomLetter =  allLetters[Math.floor(Math.random() * passwordLength)];
-     passwordString += randomLetter;
-     console.log(randomLetter)
-    // testing 
-   }
+    var randomCharArrayNum;
+    var selectedCharArray;
+    var randomCharNum;
+    var randomChar;
 
-   // if user select numbers, you need to check PasswordString has at least one number
-   if(user Number){
+    // a random number is generated for the group array (length of group array)
+    
+    randomCharArrayNum = parseInt(Math.floor(Math.random()*caseArray.length)); //random number based on length of case array
+    
+    // a random number is generated for the character array (length of character array)
 
-   }
+    selectedCharArray = caseArray[randomCharArrayNum]; //variable that stores selected array from case array
+    
+    //console.log("The array has " + caseArray.length + " entries"); //testing length of caseArray
+    //console.log(randomCharArrayNum); //testing random number generated based on length of caseArray
 
-  
-  console.log(passwordString)
-  console.log(passwordLength)
-  console.log(includeSpecialLetters)
-  return passwordString;
+    randomCharNum = Math.floor(Math.random()*selectedCharArray.length);// variable that stores random number based on length of selected array
+
+    //console.log("The selected Array is the " + selectedCharArray); //testing array chosen
+    //console.log("The selected random index is " + randomCharNum); //testing random number generation based on chosen array length 
+
+    randomChar = selectedCharArray[randomCharNum];
+    //console.log(randomChar);
+    //console.log("Your random character array num is " + randomCharArrayNum + " and your random character number is " + randomCharNum);
+
+    // now, need to accumulate the string
+    passwordString += randomChar;
+    //return passwordString;
+    
+}
+ //console.log("Your password is " + passwordString);
+//alert("Your password is " + passwordString);
+passwordEntry.textContent = passwordString;    
+
 }
 
-
-// Write password to the #password input
-function writePassword() {
-  var passwordResult = generatePassword();
-  var passTExtElement = document.querySelector("#password");
-
-  passTExtElement.value = passwordResult;
-
-}
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
+
+
+generatePassword()
